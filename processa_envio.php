63739861e59d30f815900db2d50782b2a12def24
Sérgio Bueno <84647000+Coastony@users.noreply.github.com>
2023-03-06 17:05:51 -0300

    Add files via upload

---
+++ processa_envio.php
@@ -0,0 +1,9 @@
+<?php
+ require "../../app_send_mail/bibliotecas/PHPMailer/Exception.php";
+ require "../../app_send_mail/bibliotecas/PHPMailer/OAuth.php";
+ require "../../app_send_mail/bibliotecas/PHPMailer/PHPMailer.php";
+ require "../../app_send_mail/bibliotecas/PHPMailer/POP3.php";
+ require "../../app_send_mail/bibliotecas/PHPMailer/SMTP.php";
+
+ require "../../app_send_mail/processa_envio.php";
+?>
